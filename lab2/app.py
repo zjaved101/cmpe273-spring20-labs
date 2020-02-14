2627de66e7310d13e0e7b161275708a0773445bd
Zan Javed <{ID}+{username}@users.noreply.github.com>
2020-02-13 20:58:53 -0800

    lab 2

---
+++ lab2/app.py
@@ -0,0 +1,73 @@
+from flask import Flask, escape, request, jsonify
+
+app = Flask(__name__)
+
+# STUDENTS = {
+#     "0": "John Doe",
+#     "1": "Mary Doe",
+#     "2": "Bob Doe",
+# }
+# STUDENTCOUNT = 3
+STUDENTS = ['John Doe', 'Mary Doe', 'Bob Doe']
+CLASSES = {}
+CLASSID = 0
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
+
+@app.route('/students', methods = ['POST'])
+def createStudent():
+    # STUDENTS[STUDENTCOUNT] = request.form['name']
+    # STUDENTCOUNT += 1
+    STUDENTS.append(request.form['name'])
+    return jsonify({
+        'id' : str(len(STUDENTS) - 1),
+        'name' : request.form['name']
+    }), 201
+
+@app.route('/students/<id>', methods = ['GET'])
+def getStudents(id):
+    # return "You're at the students: {} GET ROUTE".format(id)
+    return jsonify({
+        'id': id,
+        'name': STUDENTS[int(id)],
+    })
+
+@app.route('/classes', methods = ['POST'])
+def createClass():
+    global CLASSID
+    CLASSES[CLASSID] = {}
+    CLASSES[CLASSID]['name'] = request.form['name']
+    CLASSES[CLASSID]['students'] = []
+    CLASSID += 1
+    return jsonify({
+        'id' : CLASSID - 1,
+        'name' : request.form['name'],
+        'students' : CLASSES[CLASSID - 1]['students']
+    }), 201
+
+@app.route('/classes/<id>', methods = ['GET'])
+def getClass(id):
+    return jsonify({
+        'id' : id,
+        'name' : CLASSES[int(id)]['name'],
+        'students' : CLASSES[int(id)]['students']
+    })
+
+@app.route('/classes/<id>', methods = ['PATCH'])
+def patchClass(id):
+    CLASSES[int(id)]['students'].append(STUDENTS[int(request.form['student_id'])])
+    students = []
+    for student in CLASSES[int(id)]['students']:
+        students.append({
+            'id' : STUDENTS.index(student),
+            'name' : student
+        })
+    
+    return jsonify({
+        'id' : id,
+        'name' : CLASSES[int(id)]['name'],
+        'students' : students
+    })
